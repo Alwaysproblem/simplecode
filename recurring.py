565a24182873c88ebfdf1f8233e49b8eb3ff9b01
Alwaysproblem <reganyang0415@gmail.com>
2019-03-09 14:18:41 +1100

    recurring

---
+++ recurring.py
@@ -0,0 +1,16 @@
+def recurring(string):
+    b = set()
+    for i in string:
+        if i not in b:
+            b.add(i)
+        else:
+            return i
+
+def main():
+    a = input("please string:")
+    print(recurring(a))
+
+
+if __name__ == "__main__":
+    main()
+    sks
