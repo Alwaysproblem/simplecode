7f22a0bc66dabe4e3ba8dcbad529b5eeee5514bd
Alwaysproblem <reganyang0415@gmail.com>
2019-04-16 22:53:31 +1000

    add markdown

--- Lists.py
+++ Lists.py
@@ -1,4 +1,5 @@
 #%%[markdown]
+
 # Consider a list (list = []). You can perform the following commands:
 # - insert i e: Insert integer  at position.
 # - print: Print the list.
@@ -7,21 +8,25 @@
 # - sort: Sort the list.
 # - pop: Pop the last element from the list.
 # - reverse: Reverse the list. <br>
-
+#
+#
 # Initialize your list and read in the value of  followed by  lines of commands where each command will be of the  types listed above. Iterate through each command in order and perform the corresponding operation on your list.
-
+#
 # ## Input Format
-
+#
 # The first line contains an integer, $n$, denoting the number of commands.
- 
+#
 # Each line $i$ of the $n$ subsequent lines contains one of the commands scribed above.
-
+#
 # ## Constraints
-
+#
 # - The elements added to the list must be integers.
+#
+#
 # ## Output Format
-
+# 
 # For each command of type print, print the list on a new line.
+
 #%%
 if __name__ == '__main__':
     N = int(input())
