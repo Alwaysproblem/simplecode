478e867fb1b22ae7ad5326e2b951ee69aa735cf4
Alwaysproblem <reganyang0415@gmail.com>
2019-04-24 16:23:41 +1000

    inti

---
+++ interview/sockpair.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    Counter(ar)
+    print(Counter(ar))
+
+
+if __name__ == "__main__":
+    N = int(input())
+    arr = map(int, input().split())
