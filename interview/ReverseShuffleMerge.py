c23b0deb32005c72d9b7d46288a7b33ebe89d0f9
Alwaysproblem <reganyang0415@gmail.com>
2019-08-18 13:05:58 +1000

    ADD

--- interview/ReverseShuffleMerge.py
+++ interview/ReverseShuffleMerge.py
@@ -8,48 +8,59 @@ import sys
 #%%
 #%%
 
-# S = list(map(lambda x: ord(x)-97, input()))
-# S = list(reversed(S))
-# T = [0 for _ in range(26)]
-# for x in S:
-#   T[x]+=1
-# R = list(map(lambda x: x//2, T))
-# stack = []
-# for x in S:
-#   T[x]-=1
-#   if not R[x]:
-#     continue
-#   while stack and stack[-1]>x and T[stack[-1]]>R[stack[-1]]:
-#     R[stack[-1]]+=1
-#     stack.pop()
-#   if R[x]:
-#     stack.append(x)
-#     R[x]-=1
-# stack = list(map(lambda x: chr(x+97), stack))
-# print(''.join(stack))
+# https://www.hackerrank.com/challenges/reverse-shuffle-merge/problem
 
+
+class Stack(object):
+    def __init__(self, data = None):
+        if data == None:
+            self.data = []
+        else:
+            self.data = data
+    
+    def push(self, data):
+        self.data.append(data)
+
+    def pop(self):
+        return self.data.pop()
+
+    @property
+    def top(self):
+        return self.data[-1]
+
+    def isEmpty(self):
+        return len(self.data) == 0
+
+    def __repr__(self):
+        return "Stack(" + ','.join(map(str, self.data)) + ")"
 
 from collections import Counter
+# Complete the reverseShuffleMerge function below.
 def reverseShuffleMerge(s):
-    s = list(s)
-    s.reverse()
-    Cnt = Counter(s)
-    assume = Counter(s)
-    for c in Cnt.keys():
-        Cnt[c] //= 2
-    stack = []
-    for c in s:
-        assume[c] -= 1
-        if Cnt[c] == 0:
-            continue
-        while stack and stack[-1] > c and assume[stack[-1]] > Cnt[stack[-1]]:
-            a = stack.pop()
-            Cnt[a] += 1
-        if Cnt[c] != 0:
-            stack.append(c)
-            Cnt[c] -= 1
+    Cnt_A = {}
+    Cnt_S = Counter(s)
+    S_list = list(s)
+    ss = Stack()
+    assume = Cnt_S.copy()
 
-    return ''.join(stack)
+    for i in Counter(s):
+        Cnt_A[i] = Cnt_S[i] // 2
+    
+    S_list.reverse()
+    for c in S_list:
+        assume[c] -= 1
+        if Cnt_A[c] == 0:
+            continue
+        
+        while not ss.isEmpty() and ss.top > c and assume[ss.top] > Cnt_A[ss.top]:
+            a = ss.pop()
+            Cnt_A[a] += 1
+        
+        if Cnt_A[c] != 0:
+            ss.push(c)
+            Cnt_A[c] -= 1
+
+    return ''.join(ss.data)
 
 if __name__ == '__main__':
     s = input()
