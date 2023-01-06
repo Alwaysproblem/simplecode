c4129bdeb5dd46ea87855ccd857a775f8aec038c
Alwaysproblem <reganyang0415@gmail.com>
2023-01-06 11:35:34 +0800

    minDepth

--- labuladong/cpp/minDepth/main.cpp
+++ labuladong/cpp/minDepth/main.cpp
@@ -1,35 +1,59 @@
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 #include <vector>
+#include <queue>
+#include <string>
 #include <fmt/format.h>
 #include <fmt/ranges.h>
-#include <queue>
-#include <unordered_set>
-
 #include "common_types/TreeNode/BinaryTreeNode.h"
 
 using namespace std;
+using TreeNode = BinaryTree::BinaryTreeNode<int>;
+using BinaryTree::BuildBinaryTree;
+using BinaryTree::showBinaryTree;
 
 class Solution
 {
 public:
-  int BFS(string start, string target, unordered_set<string> visited)
+  int minDepth(TreeNode *root)
   {
-  }
+    if (root == nullptr) return 0;
+    if (root->left == nullptr && root->right == nullptr) return 1;
 
-  int minDepth(BinaryTreeNode<int>& root)
-  {
+    queue<TreeNode*> q;
+    q.push(root);
+    TreeNode* cur = nullptr;
 
+    int depth = 0;
+    while (!q.empty())
+    {
+      int sz = q.size();
+      for (int ix = 0; ix < sz; ++ix){
+        cur = q.front();
+        q.pop();
+        if (cur->left == nullptr && cur->right == nullptr){
+          return depth + 1;
+        }
+        if (cur->left != nullptr) q.push(cur->left);
+        if (cur->right != nullptr) q.push(cur->right);
+
+      }
+      depth ++;
+    }
+    return depth;
   }
 };
 
-int main()
+int main(int argc, char **argv)
 {
-  Solution s;
-  BinaryTreeNode<int> root{1};
-  BinaryTreeNode<int> left{1};
-  BinaryTreeNode<int> root{1};
-  int t = s.openLock(deadends, "0202");
-  fmt::print("You need to try at least {} times to open the lock.\n", t);
+  const int null = BinaryTree::null<int>();
+  auto tree = BuildBinaryTree<int>({2,null,3,null,4,null,5,null,6});
+  // auto tree = BuildBinaryTree<int>({3,9,20,null,null,15,7});
+  showBinaryTree<int>(tree);
+
+  Solution solution;
+  auto minDepth = solution.minDepth(tree);
+  fmt::print("The min depth is: {}\n", minDepth);
+
   return 0;
 }
