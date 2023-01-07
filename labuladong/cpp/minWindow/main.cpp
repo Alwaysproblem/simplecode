62997270b8a2d8e3e20f3acee014f20acfd8e472
Alwaysproblem <reganyang0415@gmail.com>
2023-01-07 13:41:03 +0000

    update 76

---
+++ labuladong/cpp/minWindow/main.cpp
@@ -0,0 +1,63 @@
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  string minWindow(string s, string t) {
+    if (s.size() == 0 || t.size() == 0) return "";
+    unordered_map<char, int> need{}, window{};
+    const int INTMAX = s.size() + 1;
+    for (char c : t) need[c]++;
+    int left = 0, right = 0;
+    char c = 0;
+    char d = 0;
+    int start = 0, len = INTMAX;
+    int valid = 0;
+    while (right < s.size()) {
+      c = s[right];
+      right++;
+
+      // window.add(right)
+      if (need.count(c)) {
+        window[c]++;
+        if (window[c] == need[c]) valid++;
+      }
+
+      fmt::print("({}, {})\n", left, right);
+
+      while (valid == need.size()) {
+        if (len > right - left) {
+          start = left;
+          len = right - left;
+        }
+        d = s[left];
+        left++;
+        // window.remove(left);
+        if (need.count(d)) {
+          if (window[d] == need[d]) valid--;
+          window[d]--;
+        }
+      }
+    }
+    fmt::print("len: {} start: {}\n", len, start);
+    return len == INTMAX ? "" : s.substr(start, len);
+  }
+};
+
+int main() {
+  Solution sol;
+  // string s = "b", t = "b";
+  // string s = "abc", t = "b";
+  // string s = "ADOBECODEBANC", t = "ABC";
+  // string s = "bba", t = "ab";
+  string s = "aa", t = "aa";
+  string v = sol.minWindow(s, t);
+  fmt::print("{}\n", v);
+  return 0;
+}
