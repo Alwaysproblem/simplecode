8af927331470b8905fb9235dc6c5fda16951966e
Alwaysproblem <reganyang0415@gmail.com>
2023-01-08 03:49:17 +0000

    add 438

---
+++ labuladong/cpp/findAnagrams/main.cpp
@@ -0,0 +1,56 @@
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> findAnagrams(string s, string p) {
+    if (!s.size() || !p.size()) return vector<int>();
+    if (s.size() < p.size()) return vector<int>();
+    unordered_map<char, int> need{}, window{};
+    for (char t : p) need[t]++;
+    int left = 0, right = 0;
+    char c = '0', d = '0';
+    int start = 0, valid = 0;
+    vector<int> starts{};
+    while (right < s.size()) {
+      c = s[right];
+      right++;
+
+      if (need.count(c)) {
+        window[c]++;
+        if (need[c] == window[c]) valid++;
+      }
+
+      fmt::print("({}, {})\n", left, right);
+
+      while (left < right && right - left == p.size()) {
+        if (valid == need.size()) {
+          start = left;
+          starts.push_back(start);
+        }
+
+        d = s[left];
+        left++;
+        if (need.count(d)) {
+          if (need[d] == window[d]) valid--;
+          window[d]--;
+        }
+      }
+    }
+    return starts;
+  }
+};
+
+int main() {
+  Solution sol;
+  string s = "abab", p = "ab";
+  vector<int> v = sol.findAnagrams(s, p);
+  fmt::print("{}\n", v);
+  return 0;
+}
