a9bbfffb78e18b2bf11724b0614c19083e4e887b
Alwaysproblem <reganyang0415@gmail.com>
2023-01-17 09:59:15 +0000

    add 25

---
+++ labuladong/cpp/reverseKGroup/main.cpp
@@ -0,0 +1,49 @@
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+#include "common_types/LinkedList/LinkedList.h"
+
+using namespace std;
+using ListNode = LinkedListNode<int>;
+
+class Solution {
+ public:
+  ListNode* reverse(ListNode* head, ListNode* target) {
+    ListNode *pre = nullptr, *cur = head, *nxt = head;
+    while (cur != target) {
+      nxt = cur->next;
+      cur->next = pre;
+      pre = cur;
+      cur = nxt;
+    }
+    return pre;
+  }
+  ListNode* reverseKGroup(ListNode* head, int k) {
+    if (!head) return head;
+    ListNode* cur = nullptr;
+    int i = 0;
+    for (cur = head; cur != nullptr && i < k; cur = cur->next, i++)
+      ;
+    if (i < k) return head;
+    ListNode* tail = reverseKGroup(cur, k);
+    ListNode* new_head = reverse(head, cur);
+    head->next = tail;
+    return new_head;
+  }
+};
+
+int main() {
+  vector<int> v = {1, 2, 3, 4, 5};
+  int k = 2;
+  ListNode* head = BuildLinkedlist<int>(v);
+  showLinkedList<int>(head);
+  Solution sol;
+  ListNode* r = sol.reverseKGroup(head, k);
+  showLinkedList<int>(r);
+  DestroyLinkedlist<int>(head);
+  return 0;
+}
