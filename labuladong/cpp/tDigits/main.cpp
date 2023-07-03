7d00e1e4c8dfb1f2f85d9c6a56070e02176f0eba
Alwaysproblem <reganyang0415@gmail.com>
2023-07-03 11:45:42 +0800

    init

---
+++ labuladong/cpp/tDigits/main.cpp
@@ -0,0 +1,267 @@
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <random>
+#include <chrono>
+
+
+// using namespace std;
+
+class Bin {
+public:
+    double avg;
+    int size;
+
+    Bin(double _avg, int _size = 1) : avg(_avg), size(_size) {}
+
+    // Override the '+' operator to implement merging two bins
+    Bin operator+(const Bin& other) const {
+        double weight = avg * size + other.avg * other.size;
+        int size = this->size + other.size;
+        return Bin(weight / size, size);
+    }
+};
+
+void printBins(std::vector<Bin>& bins) {
+    fmt::print("bins: [");
+    for (auto bin : bins) {
+        fmt::format("avg: {}, ", bin.avg, bin.size);
+    }
+    fmt::print("]\n");
+};
+
+class TDigest {
+public:
+    std::vector<Bin> bins;
+    int delta;
+
+    TDigest(std::vector<Bin> _bins = {}, int _delta = 10) : bins(_bins), delta(_delta) {
+        if (!_bins.empty()) {
+            bins = compress(_bins);
+        }
+        bins.reserve(delta);
+    }
+
+    int get_elements_count() const {
+        int count = 0;
+        for (const auto& bin : bins) {
+            count += bin.size;
+        }
+        return count;
+    }
+
+    // Add one element by converting it to a single-element t-digest then
+    // concatenating with this one.
+    void append(double value) {
+        TDigest tdigest_to_add({Bin(value)});
+        *this += tdigest_to_add;
+    }
+
+    // Override the '+=' operator to implement merging a t-digests onto
+    // the current one
+    TDigest& operator+=(const TDigest& other) {
+        // auto merged_bins = merge_bins(bins, other.bins);
+        // bins = compress(merged_bins);
+        this->merge_to_bins(other.bins);
+        printBins(this->bins);
+        this->self_compress();
+        printBins(this->bins);
+        return *this;
+    }
+
+    double get_quantile(double qid) const {
+        auto n = get_elements_count();
+        auto idx = qid * n;
+
+        auto max_idx = bins[0].size / 2.0;
+
+        if (idx < max_idx) {
+            return bins[0].avg;
+        }
+
+        for (int i = 0; i < bins.size() - 1; ++i) {
+            auto& b = bins[i];
+            auto& b_next = bins[i + 1];
+
+            auto interval_length = (b.size + b_next.size) / 2.0;
+            if (idx <= max_idx + interval_length) {
+                auto k = (idx - max_idx) / interval_length;
+                return b.avg * (1 - k) + b_next.avg * k;
+            }
+
+            max_idx += interval_length;
+        }
+
+        return bins.back().avg;
+    }
+
+private:
+    double get_potential(double qid) const {
+        return delta * asin(2 * qid - 1) / (2 * M_PI);
+    }
+
+    std::vector<Bin> merge_bins(const std::vector<Bin>& xs, const std::vector<Bin>& ys) const {
+        std::vector<Bin> merged;
+        int i = 0, j = 0;
+        while (i < xs.size() && j < ys.size()) {
+            if (xs[i].avg <= ys[j].avg) {
+                merged.push_back(xs[i]);
+                ++i;
+            } else {
+                merged.push_back(ys[j]);
+                ++j;
+            }
+        }
+
+        while (i < xs.size()) {
+            merged.push_back(xs[i]);
+            ++i;
+        }
+
+        while (j < ys.size()) {
+            merged.push_back(ys[j]);
+            ++j;
+        }
+
+        return merged;
+    }
+
+    std::vector<Bin> compress(const std::vector<Bin>& xs) const {
+        if (xs.empty()) {
+            return xs;
+        }
+
+        int n = 0;
+        for (const auto& x : xs) {
+            n += x.size;
+        }
+
+        std::vector<Bin> ys{xs[0]};
+        auto min_potential = get_potential(0);
+        auto total = xs[0].size;
+
+        for (int i = 1; i < xs.size(); ++i) {
+            const auto& x = xs[i];
+            auto next_qid = 1.0 * (total + x.size) / n;
+
+            if (get_potential(next_qid) - min_potential <= 1) {
+                ys.back() = ys.back() + x;
+            } else {
+                ys.push_back(x);
+                min_potential = get_potential(1.0 * total / n);
+            }
+
+            total += x.size;
+        }
+
+        return ys;
+    }
+
+    void merge_to_bins(const std::vector<Bin>& ys) {
+        std::vector<Bin> xs(bins);
+
+        // std::vector<Bin> merged;
+        bins.clear();
+        int i = 0, j = 0;
+        while (i < xs.size() && j < ys.size()) {
+            if (xs[i].avg <= ys[j].avg) {
+                bins.push_back(xs[i]);
+                ++i;
+            } else {
+                bins.push_back(ys[j]);
+                ++j;
+            }
+        }
+
+        while (i < xs.size()) {
+            bins.push_back(xs[i]);
+            ++i;
+        }
+
+        while (j < ys.size()) {
+            bins.push_back(ys[j]);
+            ++j;
+        }
+
+        return;
+    }
+
+    void self_compress() {
+        if (bins.empty()) {
+            return ;
+        }
+
+        std::vector<Bin> xs(bins);
+        int n = 0;
+        for (const auto& x : xs) {
+            n += x.size;
+        }
+
+        bins.clear();
+        bins.reserve(delta);
+
+        auto min_potential = get_potential(0);
+        auto total = xs[0].size;
+
+        for (int i = 1; i < xs.size(); ++i) {
+            const auto& x = xs[i];
+            auto next_qid = 1.0 * (total + x.size) / n;
+
+            if (get_potential(next_qid) - min_potential <= 1) {
+                bins.back() = bins.back() + x;
+            } else {
+                bins.push_back(x);
+                min_potential = get_potential(1.0 * total / n);
+            }
+
+            total += x.size;
+        }
+
+        return;
+    }
+
+};
+
+
+
+int main() {
+    // Create a random number generator engine
+    // std::random_device rd;
+    std::mt19937 gen(0);
+
+    // Define a distribution for the generator
+    std::uniform_int_distribution<> dis(1, 6);
+
+    // Generate a random number
+    int random_number = dis(gen);
+    const long long iters = 10;
+    // const long long iters = 1000;
+
+    std::vector<double> data;
+    data.reserve(iters);
+    for (long long i = 0; i < iters; ++i) {
+        data.push_back(dis(gen));
+    }
+
+    TDigest tdigest({}, 100);
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    for (auto d : data) {
+        tdigest.append(d);
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    // Print the result
+    std::cout << "P90 number: " << tdigest.get_quantile(0.90) << std::endl;
+
+    // Calculate the elapsed time
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+
+    // Print the result
+    fmt::print("Elapsed time: {} microseconds\n", elapsed_time.count());
+    fmt::print("Elapsed time: {} microseconds per append\n", float(elapsed_time.count()) / iters);
+
+    return 0;
+}
---
+++ labuladong/cpp/tDigits/tdigits.py
@@ -0,0 +1,124 @@
+from math import pi, asin
+
+from typing import List
+
+class Bin:
+    avg: float
+    size: int
+
+    def __init__(self, _avg: float, _size: int = 1):
+        self.avg = _avg
+        self.size = _size
+
+    # Override the '+' operator to implement merging two bins
+    def __add__(self, other: 'Bin') -> 'Bin':
+        weight = self.avg * self.size + other.avg * other.size
+        size = self.size + other.size
+        return Bin(_avg=weight/size, _size=size)
+
+
+class TDigest:
+    bins: List[Bin]
+    delta: int
+
+    def __init__(self, bins: List[Bin] = None, delta: int = 10):
+        self.delta = delta
+        self.bins = self._compress(bins) if bins is not None else []
+
+    def get_elements_count(self):
+        return sum([b.size for b in self.bins])
+
+    @staticmethod
+    def from_list(xs: List[Bin]) -> 'TDigest':
+        bins = [Bin(x) for x in xs]
+        return TDigest(bins)
+
+    def _get_potential(self, qid: float):
+        return self.delta * asin(2*qid - 1) / (2*pi)
+
+    # Add one element by converting it to a single-element t-digest then
+    # concatenating with this one.
+    def append(self, value: float):
+        tdigest_to_add = TDigest([Bin(value)])
+        self += tdigest_to_add
+
+    # Override the '+=' operator to implement merging a t-digests onto
+    # the current one
+    def __iadd__(self, other: 'TDigest') -> 'TDigest':
+        merged_bins = self._merge_bins(self.bins, other.bins)
+        new_bins = self._compress(merged_bins)
+        self.bins = new_bins
+
+    def _merge_bins(self, xs: List[Bin], ys: List[Bin]) -> List[Bin]:
+        merged = []
+        i, j = 0, 0
+        while i < len(xs) and j < len(ys):
+            if xs[i].avg <= ys[j].avg:
+                merged.append(xs[i])
+                i += 1
+            else:
+                merged.append(ys[j])
+                j +=  1
+        
+        while i < len(xs):
+            merged.append(xs[i])
+            i +=  1
+
+        while j < len(ys):
+            merged.append(ys[j])
+            j +=  1
+
+        return merged
+
+    def _compress(self, xs: List[Bin]) -> List[Bin]:
+        if len(xs) == 0:
+            return xs
+
+        n = sum([x.size for x in xs])
+        ys = [xs[0]]
+        # lowest potential of the current
+        # merged bin ys[-1]
+        min_potential = self._get_potential(0)
+        total = xs[0].size
+
+        for i in range(1, len(xs)):
+            x = xs[i]
+            next_qid = 1.0 * (total + x.size) / n
+
+            if self._get_potential(next_qid) - min_potential <= 1:
+                ys[-1] = ys[-1] + x
+            else:
+                ys.append(x)
+                min_potential = self._get_potential(1.0 * total / n)
+
+            total += x.size
+
+        return ys
+
+    def get_quantile(self, qid: float) -> float:
+        bins = self.bins
+
+        # if the elements were sorted, idx would represent the
+        # index in the array corresponding to the quantile index qid
+        idx = qid * self.get_elements_count()
+
+        max_idx = bins[0].size / 2
+
+        # idx is on the first half of the first bin
+        if idx < max_idx:
+            return bins[0].avg
+
+        for i in range(len(bins) - 1):
+            b = bins[i]
+            b_next = bins[i + 1]
+
+            interval_length = (b.size + b_next.size) / 2
+            # target index is in between b and b_next. interpolate
+            if idx <= max_idx + interval_length:
+                k = (idx - max_idx) / interval_length
+                return b.avg * (1 - k) + b_next.avg * k
+
+            max_idx += interval_length
+
+        # idx is on the second half of the last bin
+        return bins[-1].avg
