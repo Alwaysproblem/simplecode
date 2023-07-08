fbb4044e5a4a530977d29a7daa21df60673d3986
Alwaysproblem <reganyang0415@gmail.com>
2023-07-08 18:33:23 +0800

    Added 204

--- leetcode/cpp/countPrimes/main.cpp
+++ leetcode/cpp/countPrimes/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * @lc app=leetcode.cn id=204 lang=cpp
+ *
+ * [204] 计数质数
+ */
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
@@ -7,6 +12,7 @@
 
 using namespace std;
 
+// @lc code=start
 class Solution {
  public:
   int countPrimes(int n) {
@@ -19,13 +25,14 @@ class Solution {
         }
       }
     }
-    fmt::print("{}\n", isPrime);
+    // fmt::print("{}\n", isPrime);
     for (int i = 2; i < n; i++) {
       if (isPrime[i]) res += 1;
     }
     return res;
   }
 };
+// @lc code=end
 
 int main() {
   int n = 2;
