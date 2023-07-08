5f6edac3e45c724f61a8cf9c99dc71978b5624b1
Alwaysproblem <reganyang0415@gmail.com>
2023-07-08 21:30:06 +0800

    added 887

--- leetcode/cpp/superEggDrop/main.cpp
+++ leetcode/cpp/superEggDrop/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * @lc app=leetcode.cn id=887 lang=cpp
+ *
+ * [887] 鸡蛋掉落
+ */
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
@@ -7,6 +12,7 @@
 
 using namespace std;
 
+// @lc code=start
 class Solution {
  public:
   int superEggDrop(int K, int N) {
@@ -18,7 +24,7 @@ class Solution {
         dp[k][m] = dp[k][m - 1] + dp[k - 1][m - 1] + 1;
       }
     }
-    fmt::print("{}\n", fmt::join(dp.begin(), dp.end(), "\n"));
+    // fmt::print("{}\n", fmt::join(dp.begin(), dp.end(), "\n"));
     for (int i = 0; i <= N; i++) {
       if (dp[K][i] >= N) {
         return i;
@@ -27,6 +33,7 @@ class Solution {
     return 0;
   }
 };
+// @lc code=end
 
 int main() {
   int k = 10, n = 100;
