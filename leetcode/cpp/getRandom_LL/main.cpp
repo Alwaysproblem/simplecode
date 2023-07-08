2b269123d2bcc809a66974c1282053dc3cb6dcd8
Alwaysproblem <reganyang0415@gmail.com>
2023-07-08 19:38:02 +0800

    Added 382

--- leetcode/cpp/getRandom_LL/main.cpp
+++ leetcode/cpp/getRandom_LL/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * @lc app=leetcode.cn id=382 lang=cpp
+ *
+ * [382] 链表随机节点
+ */
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
@@ -10,29 +15,39 @@
 using namespace std;
 using ListNode = LinkedListNode<int>;
 
+// @lc code=start
 class Solution {
  public:
-  int getRandom(ListNode *head) {
+  ListNode *head_;
+  Solution(ListNode *head) : head_(head) {}
+
+  int getRandom(ListNode *cur_head = nullptr) {
+    if (!cur_head) {
+      cur_head = head_;
+    }
     int i = 0, res = 0;
-    for (ListNode *cur = head; cur != nullptr; cur = cur->next) {
-      i ++;
+    for (ListNode *cur = cur_head; cur != nullptr; cur = cur->next) {
+      i++;
       if (0 == (rand() % i)) {
         res = cur->val;
       }
     }
     return res;
   }
-  vector<int> getKRandom(ListNode *head, int k) {
+  vector<int> getKRandom(int k = 1, ListNode *cur_head = nullptr) {
+    if (!cur_head) {
+      cur_head = head_;
+    }
     vector<int> res(k);
-    ListNode *p = head;
-    for (int i = 0; i < k; i++){
+    ListNode *p = cur_head;
+    for (int i = 0; i < k; i++) {
       res[i] = p->val;
       p = p->next;
     }
 
     int i = 0, j = 0;
-    for (ListNode *cur = head; cur != nullptr; cur = cur->next) {
-      i ++;
+    for (ListNode *cur = cur_head; cur != nullptr; cur = cur->next) {
+      i++;
       j = rand() % i;
       if (k > j) {
         res[j++] = cur->val;
@@ -41,28 +56,29 @@ class Solution {
     return res;
   }
 };
+// @lc code=end
 
 int main() {
   vector<int> v = {1, 2, 3, 4, 5};
   ListNode *head = BuildLinkedlist<int>(v);
   showLinkedList<int>(head);
-  Solution sol;
-  int r = sol.getRandom(head);
+  Solution sol(head);
+  int r = sol.getRandom();
   fmt::print("{}\n", r);
-  r = sol.getRandom(head);
+  r = sol.getRandom();
   fmt::print("{}\n", r);
-  r = sol.getRandom(head);
+  r = sol.getRandom();
   fmt::print("{}\n", r);
-  r = sol.getRandom(head);
+  r = sol.getRandom();
   fmt::print("{}\n", r);
   int k = 3;
-  vector<int> res = sol.getKRandom(head, k);
+  vector<int> res = sol.getKRandom(k);
   fmt::print("{}\n", res);
-  res = sol.getKRandom(head, k);
+  res = sol.getKRandom(k);
   fmt::print("{}\n", res);
-  res = sol.getKRandom(head, k);
+  res = sol.getKRandom(k);
   fmt::print("{}\n", res);
-  res = sol.getKRandom(head, k);
+  res = sol.getKRandom(k);
   fmt::print("{}\n", res);
   DestroyLinkedlist<int>(head);
   return 0;
