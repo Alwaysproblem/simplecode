73140e7157adebe974e9575fcc6e8276c4aeb629
Alwaysproblem <reganyang0415@gmail.com>
2023-07-08 19:46:58 +0800

    Added 125

--- leetcode/cpp/isPalindrome/main.cpp
+++ leetcode/cpp/isPalindrome/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * @lc app=leetcode.cn id=125 lang=cpp
+ *
+ * [125] 验证回文串
+ */
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
@@ -7,6 +12,7 @@
 
 using namespace std;
 
+// @lc code=start
 class Solution {
  public:
   bool isPalindrome(string& s) {
@@ -18,7 +24,7 @@ class Solution {
         r[i] = s[j];
         i++;
       } else if (s[j] >= 'A' && s[j] <= 'Z') {
-        r[i] = s[j] | ' '; // note 'A' | ' ' = 'a'
+        r[i] = s[j] | ' ';  // note 'A' | ' ' = 'a'
         // r[i] = s[j] + ('a' - 'A');
         i++;
       } else if (s[j] >= '0' && s[j] <= '9') {
@@ -28,7 +34,7 @@ class Solution {
     }
     r[i] = 0;
     string rs = &r[0];
-    fmt::print("{}\n", rs);
+    // fmt::print("{}\n", rs);
     int f = 0, b = rs.size() - 1;
     while (f < b) {
       if (rs[f] != r[b]) return false;
@@ -38,6 +44,7 @@ class Solution {
     return true;
   }
 };
+// @lc code=end
 
 int main() {
   string s = "0P";
