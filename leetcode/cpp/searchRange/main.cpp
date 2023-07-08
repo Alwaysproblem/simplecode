ac9afcd588e26b02184fb64270a139f53e3016cc
Alwaysproblem <reganyang0415@gmail.com>
2023-07-08 21:21:17 +0800

    Add 34

--- leetcode/cpp/searchRange/main.cpp
+++ leetcode/cpp/searchRange/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=cpp
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ */
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
@@ -7,39 +12,36 @@
 
 using namespace std;
 
+// @lc code=start
 class Solution {
  public:
-  int searchLowerbound(vector<int>& nums, int target){
+  int searchLowerbound(vector<int>& nums, int target) {
     if (!nums.size()) return -1;
     int left = 0, right = nums.size() - 1;
     int mid = 0;
-    while (left <= right){
+    while (left <= right) {
       mid = left + (right - left) / 2;
-      if (nums[mid] == target){
+      if (nums[mid] == target) {
         right = mid - 1;
-      }
-      else if (nums[mid] > target){
+      } else if (nums[mid] > target) {
         right = mid - 1;
-      }
-      else if (nums[mid] < target){
+      } else if (nums[mid] < target) {
         left = mid + 1;
       }
     }
     if (left >= nums.size()) return -1;
     return target == nums[left] ? left : -1;
   }
-  int searchUpperbound(vector<int>& nums, int target){
+  int searchUpperbound(vector<int>& nums, int target) {
     int left = 0, right = nums.size() - 1;
     int mid = 0;
-    while (left <= right){
+    while (left <= right) {
       mid = left + (right - left) / 2;
-      if (nums[mid] == target){
+      if (nums[mid] == target) {
         left = mid + 1;
-      }
-      else if (nums[mid] > target){
+      } else if (nums[mid] > target) {
         right = mid - 1;
-      }
-      else if (nums[mid] < target){
+      } else if (nums[mid] < target) {
         left = mid + 1;
       }
     }
@@ -54,16 +56,8 @@ class Solution {
     res.push_back(upper);
     return res;
   }
-
-  vector<int> SearchRange(vector<int>& nums, int target) {
-    vector<int> res;
-    int lower = searchLowerbound(nums, target);
-    int upper = searchUpperbound(nums, target);
-    res.push_back(lower);
-    res.push_back(upper);
-    return res;
-  }
 };
+// @lc code=end
 
 int main() {
   Solution s;
