0afa8317982b9d035eedf627af2b788b2429e925
Alwaysproblem <reganyang0415@gmail.com>
2023-07-08 20:19:49 +0800

    Added 239

--- leetcode/cpp/maxSlidingWindow/main.cpp
+++ leetcode/cpp/maxSlidingWindow/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * @lc app=leetcode.cn id=239 lang=cpp
+ *
+ * [239] 滑动窗口最大值
+ */
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
@@ -8,13 +13,14 @@
 
 using namespace std;
 
+// @lc code=start
 template <typename T>
 class MonotonicQueue {
  public:
   deque<T> q;
   MonotonicQueue() : q(){};
   ~MonotonicQueue() = default;
-  void Print() const { fmt::print("{}\n", q); }
+  // void Print() const { fmt::print("{}\n", q); }
   void push(T& elem) {
     while (!q.empty() && q.back() < elem) q.pop_back();
     q.push_back(elem);
@@ -34,16 +40,17 @@ class Solution {
       if (i < k - 1) {
         window.push(nums[i]);
       } else {
-        window.Print();
+        // window.Print();
         window.push(nums[i]);
         res.push_back(window.max());
         window.pop(nums[i - k + 1]);
       }
     }
-    fmt::print("{}\n", res);
+    // fmt::print("{}\n", res);
     return res;
   }
 };
+// @lc code=end
 
 int main() {
   vector<int> nums = {-7, -8, 7, 5, 7, 1, 6, 0};
