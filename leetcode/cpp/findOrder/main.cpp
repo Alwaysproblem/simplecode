17c162554b130181553ecbd5108ae06a26b77f0a
Alwaysproblem <reganyang0415@gmail.com>
2023-07-16 20:27:09 +0800

    Added 210

---
+++ leetcode/cpp/findOrder/main.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=210 lang=cpp
+ *
+ * [210] 课程表 II
+ */
+
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+#include "utils/print_2d.hpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+ public:
+  void buildGraph(vector<vector<int>>& graph, int numCourses,
+                  vector<vector<int>>& prerequisites) {
+    graph.resize(numCourses);
+    for (auto& edge : prerequisites) {
+      int from = edge[1], to = edge[0];
+      // 添加一条从 from 指向 to 的有向边
+      // 边的方向是「被依赖」关系，即修完课程 from 才能修课程 to
+      graph[from].push_back(to);
+    }
+  }
+  void hasCycle_dfs(vector<vector<int>>& graph, int s, vector<bool>& visited,
+                    vector<bool>& onPath, bool& has_cycle, vector<int>& res) {
+    if (onPath[s]) {
+      // 发现环
+      has_cycle = true;
+      return;
+    }
+
+    if (visited[s]) {
+      // 已访问过,无需再访问
+      return;
+    }
+
+    // 标记当前节点为已访问
+    visited[s] = true;
+
+    // 标记当前节点在路径上
+    onPath[s] = true;
+
+    // 递归访问相邻节点
+    for (int node : graph[s]) {
+      hasCycle_dfs(graph, node, visited, onPath, has_cycle, res);
+    }
+
+    // 当前节点访问结束,移出路径
+    onPath[s] = false;
+    res.push_back(s);
+  }
+
+  vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> graph{};
+    vector<bool> visited(numCourses, false);
+    vector<bool> onPath(numCourses, false);
+    vector<int> res;
+    bool has_cycle = false;
+
+    buildGraph(graph, numCourses, prerequisites);
+    // print2D(graph);
+    for (int i = 0; i < numCourses && !has_cycle; i++) {
+      if (!visited[i]) {
+        hasCycle_dfs(graph, i, visited, onPath, has_cycle, res);
+      }
+    }
+
+    if (has_cycle) {
+      return {};
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
+  }
+};
+// @lc code=end
+
+int main() {
+  // unordered_map<int, int> v{{1, 3}, {2, 4}, {3, NULL}};
+  Solution sol;
+  vector<vector<int>> prerequisites{};
+  int numCourses = 2;
+  vector<int> v = sol.findOrder(numCourses, prerequisites);
+  fmt::print("{}\n", v);
+  return 0;
+}
