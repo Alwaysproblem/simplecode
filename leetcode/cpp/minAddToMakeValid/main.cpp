1b73d869a87d50eecf142e5d46bc54e69518c15c
Alwaysproblem <reganyang0415@gmail.com>
2023-07-25 14:53:29 +0800

    added 921

---
+++ leetcode/cpp/minAddToMakeValid/main.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=921 lang=cpp
+ *
+ * [921] 使括号有效的最少添加
+ */
+
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
+#include <iostream>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+ public:
+  int minAddToMakeValid(string s) {
+    stack<char> st;
+    vector<char> tmp;
+    unordered_map<char, char> map = {{']', '['}, {'}', '{'}, {')', '('}};
+    for (char c : s) {
+      if (st.empty())
+        st.push(c);
+      else if (c == '{' || c == '(' || c == '[') {
+        st.push(c);
+      } else {
+        while (!st.empty() && st.top() != map[c]) {
+          tmp.push_back(st.top());
+          st.pop();
+        }
+        if (st.empty())
+          st.push(c);
+        else
+          st.pop();
+        for (char c : tmp) {
+          st.push(c);
+        }
+        tmp.clear();
+      }
+    }
+    return st.size();
+  }
+};
+// @lc code=end
+
+int main() {
+  string s = "()))((";
+  Solution sol;
+  auto v = sol.minAddToMakeValid(s);
+  fmt::print("{}\n", v);
+  return 0;
+}
