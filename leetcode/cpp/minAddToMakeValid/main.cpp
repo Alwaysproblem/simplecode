094d73fd1854ea9734ca2084a35b6dd808c8b999
Alwaysproblem <reganyang0415@gmail.com>
2023-07-25 15:03:03 +0800

    update

--- leetcode/cpp/minAddToMakeValid/main.cpp
+++ leetcode/cpp/minAddToMakeValid/main.cpp
@@ -43,6 +43,33 @@ class Solution {
     }
     return st.size();
   }
+
+  int minAddToMakeValid_simple(string s) {
+    // res 记录插入次数
+    int res = 0;
+    // need 变量记录右括号的需求量
+    int need = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] == '(') {
+        // 对右括号的需求 + 1
+        need++;
+      }
+
+      if (s[i] == ')') {
+        // 对右括号的需求 - 1
+        need--;
+
+        if (need == -1) {
+          need = 0;
+          // 需插入一个左括号
+          res++;
+        }
+      }
+    }
+
+    return res + need;
+  }
 };
 // @lc code=end
 
