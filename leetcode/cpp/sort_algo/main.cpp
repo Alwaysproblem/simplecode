eae66360e19ac64ba77dbc2e09445ef08a3a0c72
Alwaysproblem <reganyang0415@gmail.com>
2023-08-13 16:34:56 +0800

    Added heap sort

--- leetcode/cpp/sort_algo/main.cpp
+++ leetcode/cpp/sort_algo/main.cpp
@@ -18,9 +18,9 @@ class Solution {
   vector<int> bubble_sort(vector<int>& num) {
     vector<int> nums(num);
     int n = nums.size();
-    for (int i = 0; i < n; i++){
-      for (int j = 0; j < n - i - 1; j++){
-        if (nums[j] > nums[j + 1]){
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        if (nums[j] > nums[j + 1]) {
           swap(nums[j], nums[j + 1]);
         }
       }
@@ -31,10 +31,10 @@ class Solution {
   vector<int> insertion_sort(vector<int>& num) {
     vector<int> nums(num);
     int n = nums.size();
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
       int j = i - 1;
       int key = nums[i];
-      while (j >= 0 && nums[j] > key){
+      while (j >= 0 && nums[j] > key) {
         nums[j + 1] = nums[j];
         j--;
       }
@@ -43,31 +43,31 @@ class Solution {
     return nums;
   }
 
-  vector<int> quick_sort(vector<int>& num){
+  vector<int> quick_sort(vector<int>& num) {
     vector<int> nums(num);
     int n = nums.size();
     quick_sort_helper(nums, 0, n - 1);
     return nums;
   }
 
-  void quick_sort_helper(vector<int>& num, int low, int high){
-    if (low < high){
+  void quick_sort_helper(vector<int>& num, int low, int high) {
+    if (low < high) {
       int pivot_index = quick_sort_partition(num, low, high);
       quick_sort_helper(num, low, pivot_index - 1);
       quick_sort_helper(num, pivot_index + 1, high);
     }
   }
 
-  int quick_sort_partition(vector<int>& num, int low, int high){
+  int quick_sort_partition(vector<int>& num, int low, int high) {
     int pivot = num[high];
     int i = low - 1;
-    for (int j = low; j < high; j++){
-      if (num[j] <= pivot){
+    for (int j = low; j < high; j++) {
+      if (num[j] <= pivot) {
         i = i + 1;
         swap(num[i], num[j]);
       }
     }
-    swap(num[i+1], num[high]);
+    swap(num[i + 1], num[high]);
     return i + 1;
   }
 
@@ -111,6 +111,38 @@ class Solution {
       num[p] = temp[p - left];
     }
   }
+
+  vector<int> heap_sort(vector<int>& num) {
+    vector<int> nums(num);
+    int n = nums.size();
+    // build the heap from the bottom up.
+    for (int i = n / 2 - 1; i >= 0; i--) {
+      heapify(nums, n, i);
+    }
+    // from last element to the first.
+    // we swap the first element with the last element
+    // and heapify the whole array.
+    for (int i = n - 1; i >= 0; --i) {
+      swap(nums[0], nums[i]);
+      heapify(nums, i, 0);
+    }
+    return nums;
+  }
+
+  void heapify(vector<int>& nums, int heap_size, int root) {
+    int largest = root;
+    int left = 2 * root + 1;   // left node index
+    int right = 2 * root + 2;  // right node index
+
+    if (left < heap_size && nums[left] > nums[largest]) largest = left;
+
+    if (right < heap_size && nums[right] > nums[largest]) largest = right;
+
+    if (largest != root) {
+      swap(nums[root], nums[largest]);    // swap root and child if needed
+      heapify(nums, heap_size, largest);  // heapify the affected sub-tree
+    }
+  }
 };
 
 int main() {
@@ -124,5 +156,7 @@ int main() {
   fmt::print("quick_sort: \t{}\n", v);
   v = sol.merge_sort(nums);
   fmt::print("merge_sort: \t{}\n", v);
+  v = sol.heap_sort(nums);
+  fmt::print("heap_sort: \t{}\n", v);
   return 0;
 }
