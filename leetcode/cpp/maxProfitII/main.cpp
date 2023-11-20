1c0b3f7b2308481dc07577cb3c4a3d0d26c3717c
Alwaysproblem <reganyang0415@gmail.com>
2023-11-20 13:50:01 +0800

    formatter

--- leetcode/cpp/maxProfitII/main.cpp
+++ leetcode/cpp/maxProfitII/main.cpp
@@ -18,25 +18,25 @@ using namespace std;
 
 // @lc code=start
 /*
-* dp[n][k][1] 的含义就是：
-* 今天是第n天，至今最多进行 k 次交易 并且我现在手上持有着股票。
-* 再比如 dp[n][k][0]:
-* 今天是第n天，至今最多进行 k 次交易 并且我现在手上没有股票。
-* 
-* ```
-* base case：
-* dp[-1][...][0] = dp[...][0][0] = 0
-* dp[-1][...][1] = dp[...][0][1] = -infinity
-* 
-* 状态转移方程：
-* dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
-* dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
-* ```
-* 
-* This problem k is Infinity, so k - 1 = k = infinity.
-* dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
-* dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
-*/
+ * dp[n][k][1] 的含义就是：
+ * 今天是第n天，至今最多进行 k 次交易 并且我现在手上持有着股票。
+ * 再比如 dp[n][k][0]:
+ * 今天是第n天，至今最多进行 k 次交易 并且我现在手上没有股票。
+ *
+ * ```
+ * base case：
+ * dp[-1][...][0] = dp[...][0][0] = 0
+ * dp[-1][...][1] = dp[...][0][1] = -infinity
+ *
+ * 状态转移方程：
+ * dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+ * dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+ * ```
+ *
+ * This problem k is Infinity, so k - 1 = k = infinity.
+ * dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+ * dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+ */
 class Solution {
  public:
   int maxProfit(vector<int>& prices) {
@@ -45,8 +45,8 @@ class Solution {
     dp[0][0] = 0;
     dp[0][1] = -prices[0];
     for (int i = 1; i <= N; i++) {
-      dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i - 1]);
-      dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i - 1]);
+      dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+      dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i - 1]);
     }
     // print2D(dp);
     return dp[N][0];
@@ -55,7 +55,7 @@ class Solution {
 // @lc code=end
 
 int main() {
-  vector<int> prices = {1,2,3,4,5};
+  vector<int> prices = {1, 2, 3, 4, 5};
   // vector<int> prices = {7, 1, 5, 3, 6, 4};
   // vector<int> prices = {1};
   Solution sol;
